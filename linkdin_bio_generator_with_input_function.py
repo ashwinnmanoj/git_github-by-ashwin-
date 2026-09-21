4c5d1d8739771189948b9a9587a58ff99267f5d9
ashwin manoj <ashwinnmanoj@gmail.com>
2026-09-21 14:09:43 +0530

    Added_linkdin_bio_generate_input_function.py

---
+++ linkdin_bio_generator_with_input_function.py
@@ -0,0 +1,5 @@
+name = input("Name:")
+proffesion = input("what is your proffesion? ")
+intrest = input("what is your intrest? ")
+
+print(f"Hi i am {name} a passionate {proffesion} with a keen intrest in {intrest} lets connect! ")
