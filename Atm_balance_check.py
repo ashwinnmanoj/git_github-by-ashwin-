99655b9a4dad4e8b957625b7d8591077bcb9b657
ashwin manoj <ashwinnmanoj@gmail.com>
2026-09-22 22:30:24 +0530

    Added_atm_balance_check_pg.py

---
+++ Atm_balance_check.py
@@ -0,0 +1,11 @@
+#concept:variable,integration,operation,ifelse
+#customers account balance
+balance = 50000
+withdraw = int(input("enter the amount"))
+if withdraw<=balance:
+    balance=balance-withdraw
+    print("withdrawal has been succeded")
+    print("remaining balance",balance)
+else:
+    print("insufficent bank balance")
+
