9b08e0204d9dd96e771fe9e7fb6993cacff6038b
ashwin manoj <ashwinnmanoj@gmail.com>
2026-09-26 19:37:37 +0530

    prime_or_not_pg.py

---
+++ prime_or_not_problem_pg.py
@@ -0,0 +1,15 @@
+num = 30
+i = int(input("enter the number:"))
+limit = int(num/2)
+if num>1:
+    while i<=limit:
+        print(i)
+        if num%i==0:
+            print("its not prime")
+            break
+        i=i+1
+    else:
+        print("its prime")
+else:
+    print("enter valid number")
+    
